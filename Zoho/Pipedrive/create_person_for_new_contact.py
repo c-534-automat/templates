4fee37a3b3c27ddb986661b69180582dd350a122
dariusz-obuchowski <dariusz.obuchowski@lit534.pl>
2015-10-26 08:00:28 +0100

    Update create_person_for_new_contact.py

--- Zoho/Pipedrive/create_person_for_new_contact.py
+++ Zoho/Pipedrive/create_person_for_new_contact.py
@@ -27,37 +27,53 @@
     },
     "connector_out":"Pipedrive, v1.0",
     "new_mapper":
-      in_data = in_dict.get('response').get('result').get('Contacts').get('row').get('FL')
-      out_dict = {}
-      def set_out_dict():
-          first_name = ''
-          last_name = ''
-          phones = []
-          emails = []
-          for elem in in_data:
-              key = elem.get('val')
-              val = elem.get('content')
-              if key == 'First Name':
-                  first_name = val
-              elif key == 'Last Name':
-                  last_name = val
-              elif key == 'Phone':
-                  phones.append(val)
-              elif key == 'Mobile':
-                  phones.append(val)
-              elif key == 'Home Phone':
-                  phones.append(val)
-              elif key == 'Asst Phone':
-                  phones.append(val)
-              elif key == 'Other Phone':
-                  phones.append(val)
-              elif key == 'Email':
-                  emails.append(val)
-              elif key == 'Secondary Email':
-                  emails.append(val)
-          name = ' '.join([first_name, last_name])
-          out_dict['name'] = name
-          out_dict['phones'] = phones
-          out_dict['email'] = emails
-      set_out_dict()
+        input_mapper_data = in_dict.get(
+        'response').get('result').get('Contacts').get('row').get('FL')
+    
+    def set_mapper_data(input_mapper_data):
+        contact_names = {}
+        phones = []
+        emails = []
+        for elem in input_mapper_data:
+            key, value = get_item_list(elem)
+            set_phones(key, value, phones)
+            set_emails(key, value, emails)
+            set_first_last_names(key, value, contact_names)
+        contact_name = ' '.join([contact_names.get('first_name'), 
+            contact_names.get('last_name')])
+        mapper_data = {
+            'name': contact_name,
+            'email': emails,
+            'phone': phones
+        }
+        return mapper_data
+    
+    def set_emails(key, val, emails):
+        if key == 'Email':
+            emails.append(val)
+        elif key == 'Secondary Email':
+            emails.append(val)
+            
+    def set_first_last_names(key, val, contact_names):
+        if key == 'First Name':
+            contact_names['first_name'] = val
+        elif key == 'Last Name':
+            contact_names['last_name'] = val
+        
+    def set_phones(key, val, phones):
+        if key == 'Phone':
+            phones.append(val)
+        elif key == 'Mobile':
+            phones.append(val)
+        elif key == 'Home Phone':
+            phones.append(val)
+        elif key == 'Asst Phone':
+            phones.append(val)
+        elif key == 'Other Phone':
+            phones.append(val)
+            
+    def get_item_list(elem):
+        return elem.get('val'), elem.get('content')
+        
+    out_dict = set_mapper_data(input_mapper_data)
 }
